761c6d16ea346f170db47d431142b3141a90d919
dhruvika <purohitdhruvika87@gmail.com>
2025-12-03 23:16:18 +0530

    first commit

---
+++ modules/path/app.js
@@ -0,0 +1,20 @@
+// const path = require("path");//used to inport core model(inbuid module) in file.
+// console.log(path.basename(__filename))//give the base name of the file
+// console.log(path.dirname(__filename))//give the full directory path of file
+// console.log(path.extname(__filename))//give the extension of file
+// console.log(path.join(__dirname, "public", "index.html"));//combile the path to directory path
+
+
+
+// es module version
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// console.log(__filename);
+// console.log(__dirname);
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+// console.log(path.join(__dirname, "public", "index.html"));
