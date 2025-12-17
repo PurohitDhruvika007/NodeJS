8322730c5a3ac2794b42a6df5198ddf9380576d7
dhruvika <purohitdhruvika87@gmail.com>
2025-12-17 20:20:28 +0530

    17-12-2025

--- Database/mongoDB_Driver/server.js
+++ Database/mongoDB_Driver/server.js
@@ -4,6 +4,34 @@ import { MongoClient } from "mongodb";
 const app = express();
 app.use(express.json());
 
-const connectData = () => {
+const client = new MongoClient("mongodb://127.0.0.1:27017");
+const dbname = "student";
+
+const connectData = async () => {
+    await client.connect();
+    console.log("database connected successfully")
+    return client.db(dbname)
+}
+
+
+
+const add_student = async () => {
+    const db = await connectData();
+    const result = await db.collection("user").insertOne({
+        name: "arpita",
+        gender: "female",
+        email: "arpita12@gmail.com"
+    })
+    console.log("data added")
+    return result;
 
 }
+
+const getUsers = async () => {
+    const db = await connectData();
+    const users = await db.collection("user").find().toArray();
+    console.log("Users:", users);
+};
+
+// add_student()
+getUsers()
